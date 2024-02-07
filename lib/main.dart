ad39f8fade232e46605a11eab2d18f7b36e7d3da
Emeradonis <achraf.abdoulmoulah@gmail.com>
2024-02-07 12:29:54 +0100

    News

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class User {
@@ -12,15 +12,19 @@ class User {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: FirstPage(),
     );
   }
 }
 
 class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +40,6 @@ class FirstPage extends StatelessWidget {
         child: Container(
           height: 420,
           width: 320,
-
           decoration: BoxDecoration(
             color: Colors.grey,  // Set the background color
             borderRadius: BorderRadius.circular(10),  // Set border radius
@@ -49,51 +52,46 @@ class FirstPage extends StatelessWidget {
           color: Colors.grey.withOpacity(0.7),
             spreadRadius: 5,
             blurRadius: 7,
-            offset: Offset(0, 3),
+            offset: const Offset(0, 3),
         ),
       ]),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Edujad!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Welcome to Edujad!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Text('want to start learning?',
+              const Text('want to start learning?',
                 style: TextStyle(fontWeight: FontWeight.bold)
             ),
-            SizedBox(height: 30),
-            Text(
-              'already have an account?',
-              style: TextStyle(fontSize: 13,),
-            ),
-            const SizedBox(height: 3),
-            ElevatedButton(
-              onPressed: () {
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate to the create account page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const CreateAccountPage()),
+                  );
+                },
+                child: const Text('Create Account'),
+              ),
+            const SizedBox(height: 15),
+            const Text(
+                'already have an account?',
+                style: TextStyle(fontSize: 13,),
+              ),
+              const SizedBox(height: 3),
+              ElevatedButton(
+                onPressed: () {
                 // Navigate to the sign-in page
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SignInPage()),
+                  MaterialPageRoute(builder: (context) => const SignInPage()),
                 );
               },
-              child: Text('Sign In'),
-            ),
-            SizedBox(height: 15),
-            Text(
-              "You don't have an account",
-              style: TextStyle(fontSize: 13, )
-            ),
-          const SizedBox(height: 3),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the create account page
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CreateAccountPage()),
-                );
-              },
-              child: Text('Create Account'),
+                child: const Text('Sign In'),
             ),
           ],
         ),
@@ -103,11 +101,17 @@ class FirstPage extends StatelessWidget {
 }
 
 class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign in',
+        backgroundColor: Colors.grey[300],
+        title: Image.asset(
+          'asset/logo.png',
+          width: 100, // Adjust the width as needed
+          height: 60, // Adjust the height as needed
         ),
       ),
       body: Center(
@@ -127,32 +131,38 @@ class SignInPage extends StatelessWidget {
             color: Colors.grey.withOpacity(0.7),
             spreadRadius: 5,
             blurRadius: 7,
-            offset: Offset(0, 3),
+            offset: const Offset(0, 3),
           ),
-        ]),
+        ],),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Enter your username and password',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Enter your Username and Password',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
             // Add text fields for username and password here
-            TextField(
-              decoration: InputDecoration(labelText: 'Username'),
+            Container(
+              margin: const EdgeInsets.only(top:40, left: 20, right: 20),
+              child: const TextField(
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
             ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true, // For password fields
+            Container(
+              margin: const EdgeInsets.only(top:20, left: 20, right: 20),
+              child: const TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true, // For password fields
+              ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // Add logic for sign-in button
               },
-              child: Text('Sign In'),
+              child: const Text('Sign In'),
             ),
           ],
         ),
@@ -162,49 +172,91 @@ class SignInPage extends StatelessWidget {
 }
 
 class CreateAccountPage extends StatefulWidget {
+  const CreateAccountPage({super.key});
+
   @override
-  _CreateAccountPageState createState() => _CreateAccountPageState();
+  _CreateAccountPageState createState() {
+    return _CreateAccountPageState();
+  }
 }
 
 class _CreateAccountPageState extends State<CreateAccountPage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Create an Account')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+      appBar: AppBar(backgroundColor: Colors.grey[300],
+        title: Image.asset(
+          'asset/logo.png',
+          width: 100, // Adjust the width as needed
+          height: 60, // Adjust the height as needed
+        ),
+      ),
+      body: Center(
+        child: Container(
+          height: 420,
+          width: 320,
+          decoration: BoxDecoration(
+          color: Colors.grey,  // Set the background color
+          borderRadius: BorderRadius.circular(10),  // Set border radius
+          border : Border.all(
+          color: Colors.black,  // Set border color
+          width: 0.25,  // Set border width
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.7),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+    ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
             // Email input field
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 20),
+              const Text(
+                'Create an Account and start learning',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top:15, left: 20, right: 20),
+                child: TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                ),
+              ),
+              const SizedBox(height: 12),
             // Username input field
-            TextFormField(
-              controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              Container(
+                margin: const EdgeInsets.only(top:15, left: 20, right: 20),
+                child: TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(labelText: 'Username'),
+              ),
             ),
-            SizedBox(height: 20),
+              const SizedBox(height: 12),
             // Password input field
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+              Container(
+                margin: const EdgeInsets.only(top:15, left: 20, right: 20),
+                child: TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
                 // Implement logic to create an account
-                final email = _emailController.text;
-                final username = _usernameController.text;
-                final password = _passwordController.text;
+                  final email = _emailController.text;
+                  final username = _usernameController.text;
+                  final password = _passwordController.text;
 
                 // Validate input and proceed to account creation
                 // You can add further validation checks here
@@ -219,11 +271,12 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
                   );
                 }
               },
-              child: Text('Create Account'),
+                child: const Text('Create Account'),
             ),
           ],
         ),
       ),
+    ),
     );
   }
 }
@@ -231,24 +284,24 @@ class UserAccountPage extends StatelessWidget {
   final String username;
   final String email;
 
-  UserAccountPage({required this.username, required this.email});
+  const UserAccountPage({super.key, required this.username, required this.email});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Account Details')),
+      appBar: AppBar(title: const Text('Account Details')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Username: $username'),
             Text('Email: $email'),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // Implement logic for taking the test
               },
-              child: Text('Take the Test'),
+              child: const Text('Take the Test'),
             ),
           ],
         ),
